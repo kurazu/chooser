7a263abb38d837cd774c87799058b8eefe2e4aa4
Tomasz Maćkowiak <kurazu@kurazu.net>
2016-04-14 14:47:45 +0200

    keypress handling

--- ui.py
+++ ui.py
@@ -1,10 +1,36 @@
 import gi
 gi.require_version('Gtk', '3.0')
+gi.require_version('Gdk', '3.0')
+
 from gi.repository import Gtk
+from gi.repository import Gdk
+
+
+KEY_ACTIONS = {
+    Gdk.KEY_Escape: Gtk.main_quit
+}
+
+
+def on_keypress(widget, event, data=None):
+    keyval = event.keyval
+    try:
+        callback = KEY_ACTIONS[keyval]
+    except KeyError:
+        return False
+    else:
+        callback()
+        return True
 
 
 def run_ui(task_queue):
-    win = Gtk.Window()
+    screen_width = Gdk.Screen.width()
+    screen_height = Gdk.Screen.height()
+
+    win = Gtk.Window(type=Gtk.WindowType.TOPLEVEL)
     win.connect("delete-event", Gtk.main_quit)
-    win.show_all()
+    win.connect("key-press-event", on_keypress)
+
+    win.show()
+    win.fullscreen()
+
     Gtk.main()
